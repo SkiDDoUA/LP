5bde2962d6c1135ba1a2aa0697e8570cdd8a869b
Anton <antonlena516@gmail.com>
2022-09-03 21:28:15 +0300

    Work On Favorite Buttons

--- LP/Cells/ProductCollectionViewCell.swift
+++ LP/Cells/ProductCollectionViewCell.swift
@@ -15,10 +15,29 @@ class ProductCollectionViewCell: UICollectionViewCell {
     @IBOutlet weak var favoriteButton: UIButton!
     @IBOutlet weak var productImageView: UIImageView!
     
+    private var database: Database?
+    var product: Product!
+    var favoriteButtonChoosen = false
+    
     func configure(for product: Product) {
+        self.product = product
         self.productBrandLabel.text = product.brand.name.uppercased()
         self.productNameLabel.text = product.name
         self.productPriceLabel.text = "₴\(product.price.description)"
         self.productImageView.kf.setImage(with: URL(string: product.images[0]))
+    }
+    
+    //MARK: - Add To Favorite
+    @IBAction func addToFavoriteButtonTapped(_ sender: Any) {
+        database = Database()
+        favoriteButtonChoosen = !favoriteButtonChoosen
+
+        if favoriteButtonChoosen == true {
+            database?.addUserProduct(collection: .favorites, productReference: product.reference, size: "")
+            favoriteButton.setImage(UIImage(named: "Favorite Filled"), for: .normal)
+        } else {
+            database?.removeUserProduct(collection: .favorites, productReference: product.reference)
+            favoriteButton.setImage(UIImage(named: "Favorite"), for: .normal)
+        }
     }
 }
